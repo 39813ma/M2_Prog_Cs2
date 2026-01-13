6d41f8b93f05a7898b6219eacc4ce2eff233d99c
MirnaxvdW <marnixvanderwerff@gmail.com>
2026-01-13 12:29:06 +0100

    00

---
+++ 06_fileIo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _06_fileIo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+    }
+
+    private void Run()
+    {
+
+    }
+}
